c2ba45fd69421cb4e7dcd5fa761a20d0669a77be
LostVicious <lostnumber@libero.it>
2017-04-12 00:16:05 +0200

    minors layout optimization

--- website/rapporti.php
+++ website/rapporti.php
@@ -3,12 +3,16 @@
 <head>
 <style type="text/css">
 .tg  {border-collapse:collapse;border-spacing:0;border-color:#6E6E6E;margin:0px auto;}
-.tg td{font-family:Arial, sans-serif;font-size:15.5px;padding:10px 14px;border-style:outset;border-width:1px;overflow:hidden;word-break:normal;border-color:#948e8e;color:#8c7b7b;background-color:#222;}
-.tg th{font-family:Arial, sans-serif;font-size:15.5px;font-weight:normal;padding:10px 14px;border-style:outset;border-width:1px;overflow:hidden;word-break:normal;border-color:#6E6E6E;color:#afa6a6;background-color:#0e3252;}
+.tg td{font-family:Arial, sans-serif;font-size:15.5px;padding:12px 16px;border-style:outset;border-width:1px;overflow:hidden;word-break:normal;border-color:#948e8e;color:#8c7b7b;background-color:#222;}
+.tg th{font-family:Arial, sans-serif;font-size:15.5px;font-weight:normal;padding:15px 19px;border-style:outset;border-width:1px;overflow:hidden;word-break:normal;border-color:#6E6E6E;color:#afa6a6;background-color:#0e3252;}
 .tg .tg-yw4l{vertical-align:top}
 body {background-color: #444;}
 a {color:#12a012;
 text-decoration: none;}
+.green {
+    color: #12a012;
+}   
+    
 .red {
     color: #da2929;
 }
@@ -28,7 +32,7 @@ if ($conn->connect_error) {
      die("Connection failed: " . $conn->connect_error);
 } 
     
-echo "<table class=\"tg\"><tr><th>Codalfa</th><th>Tempo</th><th>Controvalore Totale</th><th>Controvalore 30</th><th>Numero Trade 30</th><th>Controvalore medio 30</th><th>Market Delta 30</th><th>Market Buy Percentage 30</th><th>Volatilità</th><th>Spread 30</th><th>Impatto Book Medio</th><th>Impatto Buy</th><th>Impatto Sell</th></tr>";
+echo "<table class=\"tg\"><tr><th>Codalfa</th><th>Tempo</th><th>Controvalore Totale</th><th>Controvalore 30</th><th>Num.Trade 30</th><th>Contr.Medio 30</th><th>Market Delta 30</th><th>Market Buy % 30</th><th>Volatilità</th><th>Spread 30</th><th>Impatto Book</th><th>Impatto Buy</th><th>Impatto Sell</th></tr>";
     
 #$sql = "SELECT * FROM indicatoridaily WHERE id IN (SELECT Max( id ) FROM indicatoridaily GROUP BY codalfa ) order by codalfa";
 $sql = "SELECT * FROM indicatoridaily AS i INNER JOIN (SELECT Max( id ) AS id FROM indicatoridaily GROUP BY codalfa) AS t ON t.id = i.id;";
--- website/tool.php
+++ website/tool.php
@@ -3,8 +3,8 @@
 <head>
 <style type="text/css">
 .tg  {border-collapse:collapse;border-spacing:0;border-color:#6E6E6E;margin:0px auto;}
-.tg td{font-family:Arial, sans-serif;font-size:15.5px;padding:11px 15px;border-style:outset;border-width:1px;overflow:hidden;word-break:normal;border-color:#948e8e;color:#8c7b7b;background-color:#222;}
-.tg th{font-family:Arial, sans-serif;font-size:15.5px;font-weight:normal;padding:10px 14px;border-style:outset;border-width:1px;overflow:hidden;word-break:normal;border-color:#6E6E6E;color:#afa6a6;background-color:#0e3252;}
+.tg td{font-family:Arial, sans-serif;font-size:15.5px;padding:12px 16px;border-style:outset;border-width:1px;overflow:hidden;word-break:normal;border-color:#948e8e;color:#8c7b7b;background-color:#222;}
+.tg th{font-family:Arial, sans-serif;font-size:15.5px;font-weight:normal;padding:15px 19px;border-style:outset;border-width:1px;overflow:hidden;word-break:normal;border-color:#6E6E6E;color:#afa6a6;background-color:#0e3252;}
 .tg .tg-yw4l{vertical-align:top}
 body {background-color: #444;}
 a {color:#ada0a0;
@@ -31,7 +31,7 @@ if ($conn->connect_error) {
      die("Connection failed: " . $conn->connect_error);
 } 
     
-echo "<table class=\"tg\"><tr><th>Codalfa</th><th>Tempo</th><th>Controvalore Totale</th><th>Controvalore 30</th><th>Numero Trade 30</th><th>Controvalore medio 30</th><th>Market Delta 30</th><th>Market Buy Percentage 30</th><th>Volatilità</th><th>Spread 30</th><th>Impatto Book Medio</th><th>Impatto Buy</th><th>Impatto Sell</th></tr>";
+echo "<table class=\"tg\"><tr><th>Codalfa</th><th>Tempo</th><th>Controvalore Totale</th><th>Controvalore 30</th><th>Num.Trade 30</th><th>Contr.Medio 30</th><th>Market Delta 30</th><th>Market Buy % 30</th><th>Volatilità</th><th>Spread 30</th><th>Impatto Book</th><th>Impatto Buy</th><th>Impatto Sell</th></tr>";
     
 #$sql = "SELECT * FROM indicatoridaily WHERE id IN (SELECT Max( id ) FROM indicatoridaily GROUP BY codalfa ) order by codalfa";
 $sql = "SELECT * FROM indicatoridaily AS i INNER JOIN (SELECT Max( id ) AS id FROM indicatoridaily GROUP BY codalfa) AS t ON t.id = i.id;";
@@ -42,6 +42,10 @@ if ($result->num_rows > 0) {
      // output data of each row
      while($row = $result->fetch_assoc()) {
          
+        $sql = "SELECT * FROM indicatoristorici WHERE codalfa='".$row["codalfa"]."' AND timewindow < TIME('".$row["timestamp"]."') ORDER BY timewindow DESC LIMIT 1";
+     	$result2 = $conn->query($sql);
+     	$row2 = $result2->fetch_assoc();
+         
          echo "<tr>";
          stampaCella($row["codalfa"]);
          stampaCella($row["timestamp"]);
@@ -49,7 +53,10 @@ if ($result->num_rows > 0) {
              $valore = $row[$indicatore];
              if (in_array($indicatore, array("totalturnover", "turnover30", "averageturnover30"))) {
                  $valore = number_format($valore) . " €"; 
-                 $link = creaLink($row["codalfa"], $indicatore, $valore );
+                 $link = creaLink($row["codalfa"], $indicatore, $valore);
+                 #if ($valore>=$row2[$indicatore]){$link = creaLink($row["codalfa"], $indicatore, $valore, "green" );}
+                 #else{$link = creaLink($row["codalfa"], $indicatore, $valore, "red" );}
+                 
              }
             else if ($indicatore == "marketorderdelta30") {
                  $valore = number_format($valore) . " €"; 
